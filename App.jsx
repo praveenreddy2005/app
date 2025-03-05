853cf15a0bc6f9b7d84fec3b1e724c27afdf5122
praveenreddy2005 <bandipraveenreddy29@gmail.com>
2025-03-05 12:57:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Laptops from "./Laptops";
+import "./Laptops.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Available Laptops</h1>
+            <Laptops/>
+        </div>
+    );
+}
+
+export default App;
